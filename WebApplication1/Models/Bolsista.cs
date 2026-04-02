fc527a5b2b49414b3e7890d7ae16ca85cfa73186
Estagio Acb <Estagio Acb@LABRA01>
2026-04-02 16:50:52 -0300

    Atividade semana 1 (Victor Borges)

--- WebApplication1/Models/Bolsista.cs
+++ WebApplication1/Models/Bolsista.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.EnterpriseServices.Internal;
 
 namespace WebApplication1.Models
 {
@@ -7,8 +8,37 @@ namespace WebApplication1.Models
         // EXERCÍCIO POO:
         // Com base no formulário que vocês criaram, definam as propriedades abaixo.
         // Lembrem-se de usar 'public', o tipo de dado (string, int, etc) e o { get; set; }
-
+        
+        //Atributos:
         public string Nome { get; set; }
+
+        public string Cpf  { get; set; }
+
+        public string Matricula  { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Sexo { get; set; }
+
+        //Métodos:
+        public int CalcularIdade()
+        {
+            return DateTime.Now.Year - DataNascimento.Year;
+            //int DataAtual = DateTime.Now.Year;
+            //int idade = DataAtual - DataNascimento.Year;
+            //return idade;
+
+        }
+
+        public string Nomear_Bolsista()
+        {
+            return $"Nome: {Nome} // Matricula: {Matricula}";
+            //return "Nome: " + Nome + " Matricula: " + Matricula;
+            //return Nome + " " + Matricula;
+
+            //informações ja estão presentes no código então a necessidade é apenas retorná-la.
+        }
+
 
         // TODO: Criar a propriedade para o CPF
 
